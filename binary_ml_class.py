6ad1f32ebcbd882030ec203c0b087c8fac7ba7df
Axel Darmouni <axel.darmouni@student-cs.fr>
2022-11-22 11:33:39 +0100

    made test with sklearn classif, testing with bert

---
+++ binary_ml_class.py
@@ -0,0 +1,15 @@
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+from vectorization import vectorize_data
+
+if __name__ == "__main__":
+    vect_texts, bin_labels, vect_labels = vectorize_data("data/intent-detection-train.csv")
+    train_texts, test_texts, train_labels, test_labels = train_test_split(vect_texts, bin_labels, test_size=0.33)
+    clf_list = [RandomForestClassifier(), SVC(), AdaBoostClassifier()]
+    for clf in clf_list: 
+        clf.fit(train_texts, train_labels)
+        pred_labels = clf.predict(test_texts)
+        print(accuracy_score(test_labels, pred_labels))
