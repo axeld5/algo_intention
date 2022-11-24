fee68329994ec067f73b9b8c23be7456f7597ef4
Axel Darmouni <axel.darmouni@student-cs.fr>
2022-11-24 10:03:23 +0100

    ready for merge

--- multi_ml_class.py
+++ multi_ml_class.py
@@ -8,9 +8,8 @@ from vectorization import vectorize_data
 
 if __name__ == "__main__":
     vect_texts, bin_labels, vect_labels, label_dict = vectorize_data("data/intent-detection-train.csv")
-    print(label_dict)
-    train_texts, test_texts, train_labels, test_labels = train_test_split(vect_texts, vect_labels, test_size=0.2)
-    clf_list = [RandomForestClassifier(), SVC(), AdaBoostClassifier()]
+    train_texts, test_texts, train_labels, test_labels = train_test_split(vect_texts, vect_labels, test_size=0.33)
+    clf_list = [RandomForestClassifier()]
     for clf in clf_list: 
         clf.fit(train_texts, train_labels)
         pred_labels = clf.predict(test_texts)
