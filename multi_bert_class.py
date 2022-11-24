b1ae114c59730469b532c18af997dffdc178c3cd
Axel Darmouni <axel.darmouni@student-cs.fr>
2022-11-24 10:38:01 +0100

    made bert work for this task

--- multi_bert_class.py
+++ multi_bert_class.py
@@ -4,9 +4,10 @@ import evaluate
 import torch
 
 from sklearn.model_selection import train_test_split
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, TrainingArguments, Trainer
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, TrainingArguments, Trainer, pipeline 
 
 from vectorization import encode_labels
+from metrics import pure_accuracy, penalize_luggage_lost_errors, penalize_out_scope_errors
 
 def compute_metrics(eval_pred):
     metric = evaluate.load("accuracy")
@@ -39,7 +40,7 @@ if __name__ == "__main__":
     train_dataset = ToTorchDataset(train_tokens, train_labels)
     eval_dataset = ToTorchDataset(test_tokens, test_labels)
     model = AutoModelForSequenceClassification.from_pretrained("camembert-base", num_labels=9)
-    training_args = TrainingArguments(output_dir="test_trainer", evaluation_strategy="epoch", num_train_epochs=15)
+    training_args = TrainingArguments(output_dir="test_trainer", evaluation_strategy="epoch", num_train_epochs=10)
     trainer = Trainer(
         model=model,
         args=training_args,
@@ -48,3 +49,13 @@ if __name__ == "__main__":
         compute_metrics=compute_metrics,
     )
     trainer.train()
+    classifier = pipeline(task="text-classification", model=model, tokenizer=tokenizer)
+    pred_labels = [0]*len(test_labels) 
+    for i in range(len(test_texts)):
+        prediction = classifier(test_texts[i])
+        label = int(prediction[0]['label'][-1])
+        pred_labels[i] = label
+    print(pure_accuracy(test_labels, pred_labels))
+    print(penalize_out_scope_errors(test_labels, pred_labels, label_dict))
+    print(penalize_luggage_lost_errors(test_labels, pred_labels, label_dict))
+    
