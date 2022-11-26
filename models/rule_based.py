295fba539e544931a1baa2dcbf5c287b9e1b05be
axeld5 <axel.darmouni@student-cs.fr>
2022-11-26 19:52:59 +0100

    ready for merge

--- models/rule_based.py
+++
@@ -1,8 +0,0 @@
-import spacy 
-
-nlp = spacy.load('fr_core_news_sm')
-
-doc = nlp(u"Pouvez-vous me dire comment dire «je ne parle pas beaucoup espagnol», en espagnol")
-for token in doc:
-    print(token, token.lemma_)
-
