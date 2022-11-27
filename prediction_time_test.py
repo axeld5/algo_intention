30055ba9c9ade6ffd578506569c68f4215e75a33
axeld5 <axel.darmouni@student-cs.fr>
2022-11-27 17:42:42 +0100

    made a better time_test visualisation

--- prediction_time_test.py
+++ prediction_time_test.py
@@ -1,16 +1,22 @@
 import time 
+import matplotlib.pyplot as plt 
+import pandas as pd
+import seaborn as sns
 
 from joblib import load
 
 if __name__ == "__main__":
     verbatim = ["Bonjour, quelle est la racine carree de pi"]
-    model_dict = {"bert_model":0, "log_regression":0, "random_forest":0}
+    time_dict = {"bert_model":0, "log_regression":0, "random_forest":0}
     model_saved = {}
-    for model_name in model_dict.keys():
+    for model_name in time_dict.keys():
         model_saved[model_name] = load("./saved_models/" + model_name + ".joblib")
-    for model_name in model_dict.keys():
+    for model_name in time_dict.keys():
         start = time.time()
         model = model_saved[model_name]
         model.predict(verbatim)
-        model_dict[model_name] = time.time() - start
-    print(model_dict)
+        time_dict[model_name] = time.time() - start
+    time_df = pd.DataFrame({"model_names":list(time_dict.keys()), "time_values":list(time_dict.values())})
+    sns.barplot(data=time_df, x="model_names", y="time_values")
+    plt.title("Prediction time for one verbatim (in s)")
+    plt.show()
